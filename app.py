54d3e9cd5eee3d070aa08363080c1b01039e3de0
Ivan Romero <raguirre.ivans@gmail.com>
2022-02-08 14:22:05 -0300

    agregar ciclo

--- app.py
+++ app.py
@@ -6,12 +6,8 @@
 from turtle import *
 
 shape("turtle")
-forward(100)
-right(90)
-forward(100)
-right(90)
-forward(100)
-right(90)
-forward(100)
+for i in range (4):
+    forward(500)
+    right(90)
 
 done()
