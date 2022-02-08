1aea9b5b1cc36aa77eacb2068d23ef3b8da1617f
Ivan Romero <raguirre.ivans@gmail.com>
2022-02-08 14:58:45 -0300

    dibujar estrella

--- app.py
+++ app.py
@@ -5,9 +5,22 @@
 
 from turtle import *
 
-shape("turtle")
-for i in range (4):
-    forward(500)
-    right(90)
+# shape("turtle")
+# for i in range (4):
+#     forward(500)
+#     right(90)
 
+# done()
+
+
+# dibuja una estrella
+
+color('red', 'yellow')
+begin_fill()
+while True:
+    forward(200)
+    left(170)
+    if abs(pos()) < 1:
+        break
+end_fill()
 done()
