ea15bc3a5149957d9b7a6d5df29ef881110af49d
Ivan Romero <raguirre.ivans@gmail.com>
2022-02-08 14:02:00 -0300

    draw square

---
+++ app.py
@@ -0,0 +1,17 @@
+# Python program to draw square
+# using Turtle programming
+
+### sin loop
+
+from turtle import *
+
+shape("turtle")
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+
+done()
